7dccde68f6922bb07ae568bf78daa863e7939096
Conny Lin <connylin318@gmail.com>
2020-07-07 18:40:15 -0700

    update path

--- general/add_code_package_paths.m
+++ general/add_code_package_paths.m
@@ -2,22 +2,22 @@ function add_code_package_paths(wrkgrpname)
 
 
 % ADD FUNCTION PATH
-pFH = '/Users/connylin/Dropbox/Code/Matlab';
-pL = fullfile(pFH,'Library');
+pFH = '/Users/connylin/Code';
+pL = fullfile(pFH,'matlab_lib');
 switch wrkgrpname
     case 'RL'
-        pFW = fullfile(pFH,'Library RL');
-        pFWM = fullfile(pFH,'Library RL/Modules');
-        pFun = {pL;
-                fullfile(pFW,'Modules');
-                fullfile(pFW,'Dance');
-                fullfile(pFWM,'Graphs');
-                fullfile(pFW,'Ochestra')};
+        pFW = fullfile(pFH, 'proj', 'rankin_lab');
+        % pFWM = fullfile(pFH,'Library RL/Modules');
+        pFun = {pL; pFW};
+                % fullfile(pFW,'Modules');
+                % fullfile(pFW,'Dance');
+                % fullfile(pFWM,'Graphs');
+                % fullfile(pFW,'Ochestra')};
     case 'FB'
         pFun = {pL;
-                fullfile(pFH,'Library FB','Modules')};
+                fullfile(pFH,'proj', 'fitbrains','Modules')};
     case 'PP'
         pFun = {pL;
-                fullfile(pFH,'Library PP')};
+                fullfile(pFH,'proj')};
 end
 addpath_allsubfolders(pFun);
